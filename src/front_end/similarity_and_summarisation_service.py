b291d32e7b8629a3660685545383213288f6a4d7
Glen Hopkins <glen.hopkins@arcablanca.com>
2024-05-02 16:16:28 +0100

    Refactored methods

--- src/front_end/similarity_and_summarisation_service.py
+++ src/front_end/similarity_and_summarisation_service.py
@@ -1,6 +1,5 @@
 import gradio as gr
 import pandas as pd
-import chromadb
 from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings.sentence_transformer import (
@@ -13,18 +12,9 @@ import os
 from src.constants.paths import VECTOR_DATABASE_PATH
 from src.constants.parameters import VECTOR_DATABASE_NAME
 
+load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 BASE_URL = os.getenv("BASE_URL")
-
-
-def _load_vector_database():
-    chroma_client = chromadb.PersistentClient(
-        path=VECTOR_DATABASE_PATH,
-    )
-    # collection = chroma_client.get_collection(VECTOR_DATABASE_NAME)
-    # Remove below, used for testing
-    collection = chroma_client.get_collection("langchain_database")
-    return collection
 
 
 def _load_langchain_chroma_vector_database():
@@ -39,54 +29,65 @@ def _load_langchain_chroma_vector_database():
 
 def _abstract_summarisation(relevant_docs_df: pd.DataFrame) -> pd.DataFrame:
     summariser = pipeline("summarization", model="Falconsai/text_summarization")
-    relevant_docs_df["Summary"] = relevant_docs_df["Relevant Documents"].apply(
-        summariser
-    )
+    relevant_docs_df["Summary"] = relevant_docs_df["Abstract"].apply(summariser)
     return relevant_docs_df
 
 
-def get_langchain_chat_model_response_from_query(query_text: str):
-    chroma_db = _load_langchain_chroma_vector_database()
+def _create_similar_results_dataframe(results: list):
+    doc_titles = [results[i].metadata["title"] for i in range(len(results))]
+    doc_urls = [results[i].metadata["url"] for i in range(len(results))]
+    doc_abstracts = [results[i].metadata["abstract"] for i in range(len(results))]
+    similar_docs_df = pd.DataFrame(
+        {
+            "Document Title": doc_titles,
+            "Link": doc_urls,
+            "Abstract": doc_abstracts,
+        }
+    )
+    return similar_docs_df
+
+
+def _summarise_relevant_documents(similar_docs_df: pd.DataFrame) -> str:
+    summarisation_results = _abstract_summarisation(similar_docs_df)
+    documents_summary = "\n".join(
+        f'{row["Document Title"]}\n{row["Link"]}\n{row["Summary"][0]["summary_text"]}\n'
+        for _, row in summarisation_results.iterrows()
+    )
+    return documents_summary
+
+
+def get_langchain_chat_model_response_from_query(
+    query_text: str, chroma_vector_database: Chroma
+) -> str:
     llm_model = ChatOpenAI(
         api_key=OPENAI_API_KEY,
         base_url=BASE_URL,
     )
     chain = RetrievalQA.from_chain_type(
-        llm=llm_model, chain_type="stuff", retriever=chroma_db.as_retriever()
+        llm=llm_model,
+        chain_type="stuff",
+        retriever=chroma_vector_database.as_retriever(),
     )
     response = chain(query_text)["result"]
     return response
 
 
 def retrieve_similar_docs_and_summarisation_from_query(query_text: str):
-    collection = _load_vector_database()
-    results = collection.query(
-        query_texts=[query_text],
-        n_results=5,
+    chroma_db = _load_langchain_chroma_vector_database()
+    results = chroma_db.similarity_search(query_text, k=5)
+    llm_chat_response = get_langchain_chat_model_response_from_query(
+        query_text, chroma_db
     )
-    similar_docs = results["documents"][0]
-    doc_urls = results["ids"][0]
-    similar_docs_df = pd.DataFrame(
-        {"Relevant Documents": similar_docs, "Link": doc_urls}
-    )
-    llm_chat_response = get_langchain_chat_model_response_from_query(query_text)
-    relevant_docs_summary = summarise_relevant_documents(similar_docs_df)
-    return similar_docs_df, llm_chat_response, relevant_docs_summary
-
-
-def summarise_relevant_documents(similar_docs_df: pd.DataFrame):
-    summarisation_results = _abstract_summarisation(similar_docs_df)
-    documents_summary = ""
-    for _, row in summarisation_results.iterrows():
-        documents_summary += row["Relevant Documents"] + "\n"
-        documents_summary += row["Link"] + "\n"
-        documents_summary += row["Summary"][0]["summary_text"] + "\n" + "\n"
-
-    return documents_summary
+    similar_docs_df = _create_similar_results_dataframe(results)
+    relevant_docs_summary = _summarise_relevant_documents(similar_docs_df)
+    similar_docs_snippet = similar_docs_df[["Document Title", "Link"]]
+    return similar_docs_snippet, llm_chat_response, relevant_docs_summary
 
 
 def launch_similarity_and_summarisation_service():
-    with gr.Blocks(theme=gr.themes.base.Base(), title="Ask us anything") as front_end:
+    with gr.Blocks(
+        theme=gr.themes.base.Base(), title="Ask us about Medical Research Papers!"
+    ) as front_end:
         gr.Markdown(
             """
             # Retreive relevant research papers to your query
